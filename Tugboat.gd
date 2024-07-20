f1c86dcb624402edf93b123f8d916fce013991b8
Travis Chapman <traviswchapman@gmail.com>
2024-07-20 19:09:35 -0400

    Rope based tugging

--- Tugboat.gd
+++ Tugboat.gd
@@ -4,17 +4,22 @@ const ropeSegmentPrefab = preload("res://rope_segment.tscn")
 
 const ropeLength: float = 1.34
 @export var ropeSegments: Array[RopeSegment] = []
+@export var camera: Camera3D = null
+var cameraOffset = Vector3()
 
 func _ready():
-	pass
+	cameraOffset = camera.global_position - global_position
 
 var thrust = Vector3(0, 0, 25000)
-var torque = 20000
+var torque = 200
+
+func _process(delta):
+	camera.global_position = global_position + cameraOffset
 
 func _integrate_forces(state):
 	if Input.is_action_pressed("move_forward"):
 		var forward_direction = global_transform.basis.z
-		state.apply_force(forward_direction * 25000, Vector3.ZERO)
+		state.apply_force(forward_direction * 250, Vector3.ZERO)
 		#state.apply_force(thrust.rotated(rotation.normalized(), 0))
 	var rotation_direction = 0
 	if Input.is_action_pressed("turn_right"):
